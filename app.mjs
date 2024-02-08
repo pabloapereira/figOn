ab774aba2404347d8d6cb88db0586cb615d6ef85
pabloandrei <pabloandrei0906@gmailcom.br>
2024-02-08 01:08:23 -0300

    criação da function envioInfo

--- app.mjs
+++ app.mjs
@@ -28,17 +28,18 @@ async function authenticate() {
   }
 }
 
-// Chame a função authenticate
 authenticate()
   .then((apiToken) => {
-    return getPillars(apiToken);
+    return getPillars(apiToken).then((allPillars) => envioInfo(allPillars));
   })
-  .then((pillars) => {
-    console.log("Pilares da cultura:", pillars);
-    // Faça o que você precisa com os pilares da cultura aqui
+  .then((result) => {
+    console.log("Resultado do envio de informações:", result);
   })
   .catch((error) => {
-    console.error("Erro ao autenticar ou obter pilares da cultura:", error);
+    console.error(
+      "Erro ao autenticar, obter pilares da cultura ou enviar informações:",
+      error
+    );
   });
 
 async function getPillars(apiToken) {
@@ -51,14 +52,12 @@ async function getPillars(apiToken) {
       );
       const data = await response.json();
       console.log("Dados da resposta:", data);
-      /*if (data === null || data === "") {
-        break;
-      }*/
       const pillars = data.pillars;
       allPillars = allPillars.concat(pillars);
       if (!data.next_page) break;
       currentPage++;
     }
+
     console.log("Pilares da cultura:", allPillars);
     return allPillars;
   } catch (error) {
@@ -67,35 +66,26 @@ async function getPillars(apiToken) {
   }
 }
 
-// envio das informações
-
-getPillars()
-  .then((allPillars) => {
-    return envioInfo(allPillars); // Passando allPillars como parâmetro
-  })
-  .catch((error) => {
-    console.error("Erro ao autenticar ou obter pilares da cultura:", error);
-  });
-
 async function envioInfo(allPillars) {
   try {
-    const allPillars64 = btoa(JSON.stringify(allPillars)); // Convertendo para base64
+    const concatenatedPillars = allPillars.reduce();
+
+    const concatenatedPillarsBase64 = btoa(concatenatedPillars);
 
     const response = await fetch(
       "https://instance.fique.online/webhook/merge/88d8701e-a1d6-4fee-b15b-53e90dc1d126/envia_resposta/7b56940678e89802e02e1981a8657206d639f657d4c58efb8d8fb74814799d1c001ec121c6",
       {
         method: "POST",
-        headers: {
-          answer: allPillars64,
+        body: {
+          answer: concatenatedPillarsBase64,
         },
       }
     );
     const data = await response.json();
-    const apiToken = data.api_token;
-    console.log("all pillars base 64: ", allPillars64);
-    return allPillars64;
+    console.log("Resposta do envio de informações:", data);
+    return data;
   } catch (error) {
-    console.error("Erro na autenticação:", error);
+    console.error("Erro no envio de informações:", error);
     throw error;
   }
 }
